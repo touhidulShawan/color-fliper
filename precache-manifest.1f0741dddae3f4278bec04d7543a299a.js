19b4172dfa19d5ab3d481c5a81ea0ec5cfbfd2fd
Touhidul Shawan <touhidulshawan@gmail.com>
2020-06-10 17:16:41 +0600

    Updates

---
+++ precache-manifest.1f0741dddae3f4278bec04d7543a299a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf057c1ea3552836b3e9c3a64c89b72",
+    "url": "/color-fliper/index.html"
+  },
+  {
+    "revision": "0b7d3db48a01a63baf49",
+    "url": "/color-fliper/static/css/main.4559cbae.chunk.css"
+  },
+  {
+    "revision": "a4d32709284dc5b013e7",
+    "url": "/color-fliper/static/js/2.9d3e6242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-fliper/static/js/2.9d3e6242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7d3db48a01a63baf49",
+    "url": "/color-fliper/static/js/main.cff853d2.chunk.js"
+  },
+  {
+    "revision": "7f3259a2f47e07f32488",
+    "url": "/color-fliper/static/js/runtime-main.1419f497.js"
+  }
+]);
